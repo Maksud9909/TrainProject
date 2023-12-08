44329896709a0fe998c1465a1e9569d770336b1c
maksud9909 <rmaksud822@gmail.com>
2023-12-08 12:17:16 +0500

    Апдэйт

--- src/Test4/Test1.java
+++ src/Test4/Test1.java
@@ -1,4 +1,34 @@
 package Test4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Test1 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(1);
+        list1.add(2);
+
+        System.out.println(list1.subList(0,3));
+
+        Object [] arr = list1.toArray();
+        for (Object a:
+             arr) {
+            System.out.println(a);
+        }
+
+
+
+
+
+
+
+
+    }
 }
